55239d3de921db399d7a4ad51d868632c0d98966
Nyxvale Orenth <nyxvaleorenth@gmail.com>
2025-11-25 09:15:07 +0200

    Finished main logic

--- password manager.py
+++ password manager.py
@@ -1,1 +1,86 @@
-print("Hello world")
+import json
+
+
+def load_data():
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print("File not found, createing new file...")
+        return {}
+    except json.JSONDecodeError:
+        print("File in corrupted, creating new file...")
+        return {}
+
+
+def write_data(data):
+    try:
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent=4)
+    except Exception as error:
+        print(f"Error happened while saving the data: {error}")
+
+
+def list_all_entries(data):
+    if not data:
+        print("No entries found!")
+        return
+
+    for entry in data:
+        print(
+            f"{entry} - Username: {data[entry]['username']} - Password: {data[entry]['password']}"
+        )
+
+
+def search_website():
+    pass
+
+
+def add_entry(data):
+    website = input("Enter the website name: ")
+    username = input("Enter the user name: ")
+    password = input("Enter the password: ")
+
+    data[website] = {username: password}
+
+    return data
+
+
+def delete_entry():
+    pass
+
+
+data = load_data()
+
+
+while True:
+    print(
+        "Please chose what to do",
+        "[1] List all entries",
+        "[2] Search for an entry",
+        "[3] Add new entry",
+        "[4] Delete an entry",
+        "[5] Exit",
+        sep="\n",
+    )
+    user_input = input(">>> ")
+
+    if user_input == "1":
+        list_all_entries(data)
+
+    elif user_input == "2":
+        search_website()
+
+    elif user_input == "3":
+        add_entry(data)
+
+    elif user_input == "4":
+        delete_entry()
+
+    elif user_input == "5":
+        print("Thanks for using the app")
+        break
+
+    else:
+        print("Please enter a valid choice")
