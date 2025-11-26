03f9ff601cfe017338201b93db78992c56ce41aa
Nyxvale Orenth <nyxvaleorenth@gmail.com>
2025-11-26 16:36:23 +0200

    finished refactoring main logic

--- password manager.py
+++ password manager.py
@@ -9,7 +9,7 @@ import random
 import string
 
 
-# --------------- load and save data ---------------
+# --------------- load and save data ----------------------------------------------
 def load():
     """loads the data from a data.json file. returns a dict with the data"""
     try:
@@ -35,42 +35,36 @@ def write(data):
         print(f"Error: {error}")
 
 
-# --------------- main functions ---------------
+# --------------- main functions ---------------------------------------------------
 def list_all(data):
     """list all data entries"""
     if not data:
         print("No entries found!")
         return
 
-    border()
     for entry in data:
         print(f"{entry}:")
         print(f"    Username: {data[entry]['username']}")
         print(f"    Password: {data[entry]['password']}")
-    border()
 
 
 def search(data):
     """search for a specific entry in data"""
-    border()
     entry = input("Enter the website name: ")
     if entry in data:
-        print(f"{entry}:")
+        print(f"\n{entry}:")
         print(f"    Username: {data[entry]['username']}")
         print(f"    Password: {data[entry]['password']}")
     else:
-        print(f"{entry} not found!")
-    border()
+        print(f"\n{entry} not found!")
 
 
 def add(data):
     """add a new entry to data"""
-    border()
     entry = input("Enter the website name: ")
 
     if entry in data:
         print(f"{entry} already exists!")
-        border()
         return data
 
     username = input("Enter the username: ")
@@ -79,19 +73,16 @@ def add(data):
     data[entry] = {"username": username, "password": password}
 
     print(f"{entry} added successfully!")
-    border()
 
     return data
 
 
 def delete(data):
     """delete entry from data"""
-    border()
     entry = input("Enter the website name: ")
 
     if entry not in data:
         print(f"{entry} not found!")
-        border()
         return data
 
     data.pop(entry)
@@ -100,9 +91,58 @@ def delete(data):
     return data
 
 
-# --------------- encode and decode ---------------
-# --------------- random password generation ---------------
-# --------------- text decoration ---------------
+# --------------- encode and decode ---------------------------------------------
+# --------------- random password generation ------------------------------------
+# --------------- text decoration -----------------------------------------------
 def border():
     """Draws a simple line of '='"""
     print("\n" + "=" * 60 + "\n")
+
+
+# --------------- main loop -----------------------------------------------------
+
+data = load()
+
+while True:
+    border()
+    print(
+        "Please chose what to do",
+        "[1] List all entries",
+        "[2] Search for an entry",
+        "[3] Add new entry",
+        "[4] Delete an entry",
+        "[5] Generate random password",
+        "[6] Exit",
+        sep="\n",
+    )
+    user_input = input(">>> ")
+    border()
+
+    if user_input == "1":
+        border()
+        list_all(data)
+        border()
+    elif user_input == "2":
+        border()
+        search(data)
+        border()
+    elif user_input == "3":
+        border()
+        data = add(data)
+        write(data)
+        border()
+    elif user_input == "4":
+        border()
+        data = delete(data)
+        write(data)
+        border()
+    elif user_input == "5":
+        pass
+    elif user_input == "6":
+        print("Thanks for using the app!")
+        border()
+        quit()
+    else:
+        border()
+        print("Please enter a valid choice")
+        border()
